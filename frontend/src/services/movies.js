381176541c3a9a40b039ea8348d39452d687c7b7
Mamadou Moustapha Diallo <moustaphaotf@gmail.com>
2022-06-05 19:10:52 +0000

    Refering to the online backend

--- frontend/src/services/movies.js
+++ frontend/src/services/movies.js
@@ -1,36 +1,37 @@
 import axios from 'axios';
+const hostname="https://motf-moviesapp.herokuapp.com"
 
 class MovieDataService{
     getAll(page = 0){
-        return axios.get(`http://localhost:5000/api/v1/movies?page=${page}`);
+        return axios.get(`${hostname}/api/v1/movies?page=${page}`);
     }
 
     get(id){
-        return axios.get(`http://localhost:5000/api/v1/movies/id/${id}`);
+        return axios.get(`${hostname}/api/v1/movies/id/${id}`);
     }
 
     find(query, by='title', page=0){
         //console.log(query, by);
-        return axios.get(`http://localhost:5000/api/v1/movies?${by}=${query}&page=${page}`);
+        return axios.get(`${hostname}/api/v1/movies?${by}=${query}&page=${page}`);
     }
 
     createReview(data){
-        return axios.post('http://localhost:5000/api/v1/movies/review', data);
+        return axios.post(`${hostname}/api/v1/movies/review`, data);
     }
 
     updateReview(data){
-        return axios.put('http://localhost:5000/api/v1/movies/review', data);
+        return axios.put(`${hostname}/api/v1/movies/review`, data);
     }
 
     deleteReview(id, userId){
         return axios.delete(
-            'http://localhost:5000/api/v1/movies/review',
+            `${hostname}/api/v1/movies/review`,
             {data : {review_id:id, user_id:userId}}
         );
     }
 
     getRatings(){
-        return axios.get('http://localhost:5000/api/v1/movies/ratings');
+        return axios.get(`${hostname}/api/v1/movies/ratings`);
     }
 }
 
